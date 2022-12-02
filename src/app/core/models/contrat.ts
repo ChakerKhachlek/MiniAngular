1a994c56e2c11d5e1cbf255f13aa8eb6925d959e
rimbsaad <rimbs4@yahoo.com>
2022-12-02 13:48:21 +0100

    commit message

---
+++ src/app/core/models/contrat.ts
@@ -0,0 +1,13 @@
+export class Contrat{
+
+    id!: number;
+    specialite!: string;
+    archive!: boolean;
+    montant!: number;
+    dateDebutContrat!: Date;
+    dateFinContrat!:Date;
+}
+
+
+  
+  
