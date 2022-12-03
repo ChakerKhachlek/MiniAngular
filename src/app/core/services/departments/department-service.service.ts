9051f2d797bdf3b6e416a9137f2ab651a9097d40
houba213 <iheb.bentemessek@esprit.tn>
2022-12-03 17:36:38 +0100

    Update works

--- src/app/core/services/departments/department-service.service.ts
+++ src/app/core/services/departments/department-service.service.ts
@@ -10,12 +10,18 @@ export class DepartmentServiceService {
   
   departmentlist: Department[]= [];
   constructor(private http: HttpClient) { }
-  url = "http://localhost:8089/kaddem/Departement/retrieve-all-departements";
+  url = "http://localhost:8089/kaddem/Departement/";
+  listDepartments:Department[] = [];
 
   getAllDepartments(){
-    return this.http.get<Department[]>(this.url);
+    return this.http.get<Department[]>(this.url+'retrieve-all-departements');
    }
-
+   addDepartment(departement:Department){
+    return this.http.post(this.url+'add-Department',departement);
+   }
+   deleteDepartment(id:Number){
+    return this.http.delete(this.url+'remove-department/'+id);
+  }
    
    
 }
--- src/app/manage-departments/manage-departments.component.ts
+++ src/app/manage-departments/manage-departments.component.ts
@@ -10,22 +10,23 @@ import { Router } from 'express';
 })
 export class ManageDepartmentsComponent implements OnInit {
   listDepartments:Department[] = [];
-  //createMode : boolean ;
+  createMode : boolean ;
   //updateMode : boolean ;
 
-   //listdepartment:Department[] ;
+   listdepartment:Department[] ;
  // selectedDepartment: Department;
 
   
 
   constructor(private departmentService : DepartmentServiceService) { 
-  //  this.listDepartments=[];
+    
   }
 
   ngOnInit(): void {
     this.getDepartments();
-   // this.createMode=false;
+    this.createMode=true;
    // this.updateMode=false;
+   this.listDepartments=[];
   }
 
   getDepartments(){
@@ -34,7 +35,21 @@ export class ManageDepartmentsComponent implements OnInit {
       console.log(data);
       
     });
+  
+}
+deleteDepartment(dep:Department){
+  this.departmentService.deleteDepartment(dep.idDepart).subscribe(function(dep,data) {
+    console.log(data);
+    this.removeElementFromArray(dep);
+    }.bind(this,dep));
+  
+ }
+ removeElementFromArray(element: Department) {
+  this.listDepartments.forEach((value,index)=>{
+      if(value==element) this.listDepartments.splice(index,1);
+  });}
 
 
+}
 
-}}
+
