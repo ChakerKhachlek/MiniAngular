12c0afeede15370e63a3a0d59d977fe1d14f0abf
OussemaChaabene <99026165+OussemaChaabene@users.noreply.github.com>
2022-12-14 01:50:47 +0100

    pdf

--- src/app/manage-equipes/manage-equipes.component.ts
+++ src/app/manage-equipes/manage-equipes.component.ts
@@ -1,9 +1,11 @@
-import { Component, Input } from '@angular/core';
+import { Component, ElementRef, Input, ViewChild } from '@angular/core';
 import { FormArray,FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { DetailsEquipe } from 'app/core/models/detailsEquipe';
 import { Equipe } from 'app/core/models/equipe';
 import { EquipeService } from 'app/core/services/equipe/equipe.service';
 import { NotificationServiceService } from 'app/core/services/notification-service.service';
+import jsPDF from 'jspdf';
+import "jspdf-autotable";
 
 @Component({
   selector: 'app-manage-equipes',
@@ -57,7 +59,7 @@ export class ManageEquipesComponent {
       this.EquipeS.deleteEquipe(e).subscribe(function(e,data) {
         console.log(data);
         this.removeElementFromArray(e);
-        
+        this.getAllEquipes;
         this.notification.showNotification('top','right','Equipe supprimé avec succès !','danger');
         }.bind(this,e));
     
@@ -86,18 +88,27 @@ export class ManageEquipesComponent {
        
     
         console.log("XXX",equipeD,equipe);
+
         this.EquipeS.createEquipe(equipe).subscribe(equipe => this.listEquipes.push(equipe as Equipe));
         this.notification.showNotification('top','right','Equipe ajouté avec succès !','success');
        
     }
   }
 
-    pushEquipeForUpdate(e : Equipe){
+  equipeToModify :Equipe;
+  n : number;
+  r : number;
+    pushEquipeForUpdate(eq : Equipe){
+      console.log("puched equipe",eq.idEquipe);
       this.updateB= true;
-      this.reactiveForm.get('nom').setValue(e.nomEquipe);
-      this.reactiveForm.get('niveau').setValue(e.niveau);
-      this.reactiveForm.get('thematique').setValue(e.detailEquipe.thematique);
-      this.reactiveForm.get('salle').setValue(e.detailEquipe.salle.toString());
+      this.reactiveForm.get('nom').setValue(eq.nomEquipe);
+      this.reactiveForm.get('niveau').setValue(eq.niveau);
+      this.reactiveForm.get('thematique').setValue(eq.detailEquipe.thematique);
+      this.reactiveForm.get('salle').setValue(eq.detailEquipe.salle.toString());
+
+      this.n=eq.idEquipe;
+      this.r=eq.detailEquipe.id_detail_equipe;
+
 
     }
 
@@ -107,27 +118,39 @@ export class ManageEquipesComponent {
       this.reactiveForm.reset();
     }
 
-    updateEquipe(e :any){
+    updateEquipeAndDetailEquipe(){
       if(this.reactiveForm.valid){
-        console.log(e);
         let newEquipe=new Equipe();
-   
+        let newdetaiEquipe= new DetailsEquipe();
         newEquipe.nomEquipe = this.reactiveForm.get('nom').value;
         newEquipe.niveau = this.reactiveForm.get('niveau').value;
-        //newEquipe.detailEquipe.salle= +this.reactiveForm.get('salle').value;
-        //newEquipe.detailEquipe.thematique= this.reactiveForm.get('thematique').value;
-        newEquipe.idEquipe=e.idEquipe;
+        newdetaiEquipe.salle= +this.reactiveForm.get('salle').value;
+        newdetaiEquipe.thematique= this.reactiveForm.get('thematique').value;
+        newEquipe.idEquipe=this.n;
+        newdetaiEquipe.id_detail_equipe=this.r;
     
-        this.EquipeS.updateEquipe(newEquipe).subscribe(function(e,newEquipe) {
+        this.EquipeS.updateDetailEquipe(newdetaiEquipe).subscribe();
+        this.EquipeS.updateEquipe(newEquipe).subscribe();
+        this.getlistEquipes;
+       /* this.EquipeS.updateEquipe(newEquipe).subscribe(function(e,newEquipe) {
 
           this.updateElementFromArray(e,newEquipe as Equipe);
         
           this.requested.emit(newEquipe);
           console.log(this.updateMode,this.createMode);
-        }.bind(this,this.selectedEquipe) );
+        }.bind(this,this.selectedEquipe) );*/
         
       }
     }
+
+
+
+  download() {
+    let doc = new jsPDF();
+    (doc as any).autoTable({html: '#table'});
+    (doc as any).output('datauri','test.pdf');
+  }
+
   }
  
 
